30f48304ab3e52c3de6dd57d5cdbc35eaf13725e
awesomerobot <kris.aubuchon@discourse.org>
2022-03-22 22:06:01 -0400

    attempt to add user slide

--- javascripts/discourse/components/big-carousel.js
+++ javascripts/discourse/components/big-carousel.js
@@ -3,52 +3,65 @@ import { computed } from "@ember/object";
 import loadScript from "discourse/lib/load-script";
 import discourseComputed from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
-import { iconNode } from "discourse-common/lib/icon-library";
+import { ajax } from "discourse/lib/ajax";
+import { set } from "@ember/object";
+
+function userSlides(slide, that) {
+  // set up user slides
+  let bigUserSlides = [];
+
+  if (slide.slide_type === "user") {
+    ajax(`/u/${slide.link}.json`).then(function (result) {
+      set(slide, "user_info", result);
+    });
+    ajax(`/u/${slide.link}/summary.json`)
+      .then(function (result) {
+        set(slide, "user_summary", result);
+        bigUserSlides.push(slide);
+      })
+      .then(function () {
+        that.set("bigUserSlides", bigUserSlides);
+      });
+  }
+}
 
 export default Component.extend({
   router: service(),
   bigSlides: computed(function () {
     return JSON.parse(settings.big_carousel_slides);
   }),
-
-  @discourseComputed()
-  bigStaticSlides() {
-    let bigStaticSlides = [];
-    this.bigSlides.forEach((slide) => {
-      if (slide.slide_type === "slide") {
-        bigStaticSlides.push(slide);
-      }
-    });
-    return bigStaticSlides;
-  },
-
-  @discourseComputed()
-  bigUserSlides() {
-    let bigUserSlides = [];
-    this.bigSlides.forEach((slide) => {
-      if (slide.slide_type === "user") {
-        bigUserSlides.push(slide);
-      }
-    });
-    return bigUserSlides;
-  },
+  isLoading: true,
 
   ensureSlider() {
+    // set up up manually populated slides
+    let bigStaticSlides = [];
+    this.set("bigStaticSlides", bigStaticSlides);
     if (this.shouldDisplay && this.bigSlides.length > 1) {
-      return loadScript(settings.theme_uploads.tiny_slider).then(() => {
-        var slider = tns({
-          container: ".custom-big-carousel-slides",
-          items: 1,
-          controls: true,
-          autoplay: settings.big_carousel_autoplay,
-          speed: settings.big_carousel_speed,
-          prevButton: ".custom-big-carousel-prev",
-          nextButton: ".custom-big-carousel-next",
-          navContainer: ".custom-big-carousel-nav",
-        });
-
-        console.log(slider.getInfo()); // temp
+      this.bigSlides.forEach((slide) => {
+        if (slide.slide_type === "slide") {
+          bigStaticSlides.push(slide);
+        }
       });
+
+      Promise.all(this.bigSlides.map((slide) => userSlides(slide, this)))
+        .then(() => {
+          this.set("isLoading", false);
+        })
+        .then(() => {
+          // slider script
+          loadScript(settings.theme_uploads.tiny_slider).then(() => {
+            var slider = tns({
+              container: ".custom-big-carousel-slides",
+              items: 1,
+              controls: true,
+              autoplay: settings.big_carousel_autoplay,
+              speed: settings.big_carousel_speed,
+              prevButton: ".custom-big-carousel-prev",
+              nextButton: ".custom-big-carousel-next",
+              navContainer: ".custom-big-carousel-nav",
+            });
+          });
+        });
     }
   },
 
