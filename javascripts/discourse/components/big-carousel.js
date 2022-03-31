4aedb4bacca9bcb011fefc039d7448ac722d6715
awesomerobot <kris.aubuchon@discourse.org>
2022-03-30 23:16:00 -0400

    Get this thing actually working

--- javascripts/discourse/components/big-carousel.js
+++ javascripts/discourse/components/big-carousel.js
@@ -6,25 +6,6 @@ import { inject as service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
 import { set } from "@ember/object";
 
-function userSlides(slide, that) {
-  // set up user slides
-  let bigUserSlides = [];
-
-  if (slide.slide_type === "user") {
-    ajax(`/u/${slide.link}.json`).then(function (result) {
-      set(slide, "user_info", result);
-    });
-    ajax(`/u/${slide.link}/summary.json`)
-      .then(function (result) {
-        set(slide, "user_summary", result);
-        bigUserSlides.push(slide);
-      })
-      .then(function () {
-        that.set("bigUserSlides", bigUserSlides);
-      });
-  }
-}
-
 export default Component.extend({
   router: service(),
   bigSlides: computed(function () {
@@ -35,7 +16,7 @@ export default Component.extend({
   ensureSlider() {
     // set up up manually populated slides
     let bigStaticSlides = [];
-    this.set("bigStaticSlides", bigStaticSlides);
+
     if (this.shouldDisplay && this.bigSlides.length > 1) {
       this.bigSlides.forEach((slide) => {
         if (slide.slide_type === "slide") {
@@ -43,26 +24,63 @@ export default Component.extend({
         }
       });
 
-      Promise.all(this.bigSlides.map((slide) => userSlides(slide, this)))
-        .then(() => {
-          this.set("isLoading", false);
-        })
-        .then(() => {
-          // slider script
-          loadScript(settings.theme_uploads.tiny_slider).then(() => {
-            var slider = tns({
-              container: ".custom-big-carousel-slides",
-              items: 1,
-              controls: true,
-              autoplay: settings.big_carousel_autoplay,
-              speed: settings.big_carousel_speed,
-              prevButton: ".custom-big-carousel-prev",
-              nextButton: ".custom-big-carousel-next",
-              navContainer: ".custom-big-carousel-nav",
-            });
+      // set up user slides
+      let bigUserSlides = [];
+
+      let combinedUsers = new Promise((resolve, reject) => {
+        let count = 0;
+        let total = this.bigSlides.reduce(function (n, slide) {
+          return n + (slide.slide_type === "user");
+        }, 0);
+
+        if (total) {
+          this.bigSlides.map((slide) => {
+            if (slide.slide_type === "user") {
+              ajax(`/u/${slide.link}.json`).then(function (result) {
+                set(slide, "user_info", result);
+              });
+              ajax(`/u/${slide.link}/summary.json`)
+                .then(function (result) {
+                  set(slide, "user_summary", result);
+                  bigUserSlides.push(slide);
+                })
+                .then(function () {
+                  count++;
+
+                  if (count === total) {
+                    resolve(bigUserSlides);
+                  }
+                });
+            }
+          });
+        } else {
+          // skip if no slides
+          resolve(bigUserSlides);
+        }
+      });
+
+      Promise.all([combinedUsers]).then(() => {
+        this.set("isLoading", false);
+
+        this.set("bigUserSlides", bigUserSlides);
+
+        // slider script
+        loadScript(settings.theme_uploads.tiny_slider).then(() => {
+          var slider = tns({
+            container: ".custom-big-carousel-slides",
+            items: 1,
+            controls: true,
+            autoplay: settings.big_carousel_autoplay,
+            speed: settings.big_carousel_speed,
+            prevButton: ".custom-big-carousel-prev",
+            nextButton: ".custom-big-carousel-next",
+            navContainer: ".custom-big-carousel-nav",
           });
         });
+      });
     }
+
+    this.set("bigStaticSlides", bigStaticSlides);
   },
 
   @discourseComputed("router.currentRouteName")
